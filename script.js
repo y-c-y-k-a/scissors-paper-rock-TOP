678e3fd949b948c78c4823e067ca2687937a21cf
y-c-y-k-a <183938902+y-c-y-k-a@users.noreply.github.com>
2026-06-01 12:00:55 +0400

    Add getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('Hello world');
+let computerWins = 0;
+let playerWins = 0;
+
+function getComputerChoice() {
+  let choice = Math.random();
+  if (choice < 0.33) {
+    return 'Scissors';
+  } else if (choice > 0.33 && choice < 0.66) {
+    return 'Paper';
+  } else {
+    return 'Rock';
+  }
+}
