d07978e7f1e901fd17ff45ed996e01053976b9d4
y-c-y-k-a <183938902+y-c-y-k-a@users.noreply.github.com>
2026-06-01 12:34:23 +0400

    Add playRound() function

--- script.js
+++ script.js
@@ -1,17 +1,47 @@
-let computerWins = 0;
-let playerWins = 0;
-
 function getComputerChoice() {
   let choice = Math.random();
   if (choice < 0.33) {
-    return 'Scissors';
+    return 'scissors';
   } else if (choice > 0.33 && choice < 0.66) {
-    return 'Paper';
+    return 'paper';
   } else {
-    return 'Rock';
+    return 'rock';
   }
 }
 
 function getHumanChoice() {
   return prompt('What is your choice?', 'Scissors');
 }
+
+function playRound(humanChoice, computerChoice) {
+  let lowerCaseHumanChoice = humanChoice.toLowerCase();
+  let lowerCaseComputerChoice = computerChoice.toLowerCase();
+
+  if (lowerCaseHumanChoice === 'scissors' && lowerCaseComputerChoice === 'scissors'
+    || lowerCaseHumanChoice === 'paper' && lowerCaseComputerChoice === 'paper'
+    || lowerCaseHumanChoice === 'rock' && lowerCaseComputerChoice === 'rock'
+  ) {
+    return "It's a draw!";
+  } else if (lowerCaseHumanChoice === 'scissors' && lowerCaseComputerChoice === 'paper') {
+    playerWins++;
+    return 'Scissors cuts paper, you win!';
+  } else if (lowerCaseHumanChoice === 'scissors' && lowerCaseComputerChoice === 'rock') {
+    computerWins++;
+    return 'Rock beats scissors, you lose!';
+  } else if (lowerCaseHumanChoice === 'paper' && lowerCaseComputerChoice === 'scissors') {
+    computerWins++;
+    return 'Scissors cuts paper, you lose!';
+  } else if (lowerCaseComputerChoice === 'paper' && lowerCaseComputerChoice === 'rock') {
+    playerWins++;
+    return 'Paper wraps rock, you win!';
+  } else if (lowerCaseHumanChoice === 'rock' && lowerCaseComputerChoice === 'scissors') {
+    playerWins++;
+    return 'Rock beats scissors, you win!';
+  } else if (lowerCaseHumanChoice === 'rock' && lowerCaseComputerChoice === 'paper') {
+    computerWins++;
+    return 'Paper wraps rock, you lose!';
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
